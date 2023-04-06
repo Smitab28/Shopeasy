5bb72de6d640d9c074180f63e9434985a03f6ac0
Smitab28 <smitabhor2894@gmail.com>
2023-04-06 13:05:56 +0530

    category-CRUD

--- src/main/java/com/shopeasy/controller/CategoryController.java
+++ src/main/java/com/shopeasy/controller/CategoryController.java
@@ -1,14 +1,21 @@
 package com.shopeasy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shopeasy.entity.Category;
+import com.shopeasy.entity.Product;
 import com.shopeasy.service.CategoryService;
 
 @Controller
@@ -21,6 +28,43 @@ public class CategoryController {
 	@PostMapping("/save-category")
 	public ResponseEntity<Boolean> saveCategory(@RequestBody Category category){
 		boolean isSaved = service.saveCategory(category);
-		return new ResponseEntity<>(isSaved, HttpStatus.CREATED);
+		if(isSaved)
+			return new ResponseEntity<>(isSaved, HttpStatus.CREATED);
+		else
+			return new ResponseEntity<>(isSaved, HttpStatus.ALREADY_REPORTED);
+	}
+	
+	@PutMapping("/update-category")
+	public ResponseEntity<Boolean> updateCategory(@RequestBody Category category){
+		boolean isUpdated = service.updateCategory(category);
+		if(isUpdated)
+			return new ResponseEntity<>(isUpdated, HttpStatus.ACCEPTED);
+		else
+			return new ResponseEntity<>(isUpdated, HttpStatus.NOT_MODIFIED);
+	}
+	
+	@DeleteMapping("/delete-category{/categoryId}")
+	public ResponseEntity<Boolean> deleteCategory(@PathVariable int categoryId){
+		boolean isDeleted = service.deleteCategory(categoryId);
+		if(isDeleted) {
+			return new ResponseEntity<>(isDeleted, HttpStatus.FOUND);
+		}
+		return new ResponseEntity<>(isDeleted, HttpStatus.NOT_FOUND);
+	}
+	
+	@GetMapping("/get-singlecategory/{categorytId}")
+	public ResponseEntity<Category> getSingleProductById(@PathVariable int categoryId){
+		Category category = service.getCategorybyId(categoryId);
+		if(category!=null) {
+			return new ResponseEntity<>(category, HttpStatus.ACCEPTED);
+		}
+		else
+			return new ResponseEntity<>(category, HttpStatus.NOT_FOUND);
+	}
+	
+	@GetMapping("/get-all-catgory")
+	public ResponseEntity<List<Category>> getAllCategory(){
+		List<Category> list = service.getAllCategory();
+		return new ResponseEntity<List<Category>>(list, HttpStatus.NOT_FOUND);
 	}
 }
--- src/main/java/com/shopeasy/daoIMPL/CategoryDaoIMPL.java
+++ src/main/java/com/shopeasy/daoIMPL/CategoryDaoIMPL.java
@@ -45,26 +45,76 @@ public class CategoryDaoIMPL implements CategoryDao {
 
 	@Override
 	public boolean updateCategory(Category category) {
-		// TODO Auto-generated method stub
-		return false;
+		Session session = null;
+		boolean isUpdated = false;
+		try {
+			session = sf.openSession();
+			Criteria criteria = session.createCriteria(Category.class);
+			List<Category> list = criteria.list();
+			for (Category category2 : list) {
+				if(category2.getCategoryId()==category.categoryId) {
+					int index = list.indexOf(category2);
+					list.set(index, category);
+					isUpdated = true;
+				}				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return isUpdated;
 	}
 
 	@Override
 	public boolean deleteCategory(int categoryId) {
-		// TODO Auto-generated method stub
-		return false;
+		Session session = null;
+		boolean isDeleted = false;
+		try {
+			session = sf.openSession();
+			Criteria criteria = session.createCriteria(Category.class);
+			List<Category> list = criteria.list();
+			for (Category category : list) {
+				if(category.getCategoryId()==categoryId) {
+					int index = list.indexOf(category);
+					list.remove(index);
+					isDeleted = true;
+				}
+			}
+			 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return isDeleted;
 	}
 
 	@Override
 	public Category getCategorybyId(int categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		Category category =null;
+		try {
+			session= sf.openSession();
+			Criteria criteria = session.createCriteria(Category.class);
+			List<Category> list = criteria.list();
+			category = list.stream().filter(val -> val.categoryId==categoryId).findFirst().get();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return category;
 	}
 
 	@Override
 	public List<Category> getAllCategory() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		List<Category> list =null;
+		try {
+			session= sf.openSession();
+			Criteria criteria = session.createCriteria(Category.class);
+			 list = criteria.list();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
-
 }
+
